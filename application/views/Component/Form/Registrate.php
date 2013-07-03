754e2ce737a2b668dc03b90dd5a61b65b1462195
ssstrz <ssstrz@gmail.com>
2013-07-03 22:28:39 +0200

    registration

--- application/views/Component/Form/Registrate.php
+++ application/views/Component/Form/Registrate.php
@@ -1,42 +1,56 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
 <?php $style=(isset($style)?$style:null);?>
-  <form>
-        <div <?php echo HTML::attributes($style);?>>
-        
-            <div class="well">
-                <h4>Dane wymagane</h4>
-                <div class="row-fluid">
-                    <div class="span12">
-                        <label for="email">
-                            e-mail:
-                            <label class="checkbox">
-                                <input type="checkbox" checked><small>widoczny publicznie</small>
-                            </label>
-                        </label>
-                        <input name="email" type="email" class="span8" placeholder="email" />
-                        <span class="label label-warning">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1</span>
+<?php $error=(isset($error)?$error:null);?>
+<?php echo Form::open(Route::get('default')
+		->uri(array('Controller'=>'welcome', 'action'=>'registrate')),array(HTML::attributes($style)));?>
+		<div>
+			<div class="well">
+				<h4><?php echo __('Needed data');?></h4>
+				<div class="row-fluid">
+					<div class="span12">
+						<label for="email">
+							e-mail:
+							<label class="checkbox">zawsze widoczny
+<?php echo Form::input('show_email',null, array('checked', 'type'=>'checkbox'));?>
+							</label>
+						</label>
+<?php echo Form::input('email', '', array('type'=>'email','class'=>'span8', 'placeholder'=>'email'));?>
+						<span class="label label-warning"><?php echo Arr::get($error, 'email');?></span>
                         <hr>
-                    </div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span12">
-                        <label for="username">username:</label>
-                        <input name="username" type="text" class="span8" placeholder="nazwa użytkownika" />
-                    </div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span12">
-                        <label for="password">hasło:</label>
-                        <input name="password" type="password" class="span8" placeholder="hasło" />
-                    </div>
-                </div>
-                <div class="row-fluid">
-                    <div class="span12">
-                        <label for="password_confirm">hasło potwierdź:</label>
-                        <input name="password_confirm" type="password" class="span8" placeholder="hasło - potwierdź" />
-                    </div>
-                </div>
-                <hr>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span12">
+						<label for="username">
+							username:
+						</label>
+						<?php echo Form::input('username', '', array('class'=>'span8', 'placeholder'=>'username'));?>
+						<span class="label label-warning"><?php echo Arr::get($error, 'username');?></span>
+                        <hr>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span12">
+						<label for="pasword">
+							password:
+						</label>
+<?php echo Form::input('password', '', array('type'=>'password', 'class'=>'span8', 'placeholder'=>'password'));?>
+						<span class="label label-warning"><?php echo Arr::get($error, 'password');?></span>
+                        <hr>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span12">
+						<label for="pasword_confirm">
+							password_confirm:
+						</label>
+<?php echo Form::input('password_confirm', '', array('type'=>'password', 'class'=>'span8', 'placeholder'=>'_confirm'));?>
+						<span class="label label-warning"><?php echo Arr::get($error, 'password');?></span>
+                        <hr>
+					</div>
+				</div>
+				<hr>
+               <hr>
                 <h5>Dane dodatkowe:</h5>
                 <div class="row-fluid">
                     <div class="span12">
@@ -68,4 +82,5 @@
             <a href="#" class="btn" data-dismiss="modal" aria-hidden="true">Close</a>
             <a  href="#" class="btn btn-primary">Zarejestruj się</a>
         </div>
-    </form>
+<?php echo Form::close();?>
+
