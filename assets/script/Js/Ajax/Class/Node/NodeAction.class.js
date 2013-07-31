e3454f68ba32707f7da66fe8dca1b0cec4ac35ee
ssstrz <ssstrz@gmail.com>
2013-07-31 14:34:44 +0200

    js formatting

--- assets/script/Js/Ajax/Class/Node/NodeAction.class.js
+++ assets/script/Js/Ajax/Class/Node/NodeAction.class.js
@@ -2,57 +2,96 @@
 	strz_Ajax.NodeAction = function() {
 		this.init();
 	};
+	/**
+	 * Public fields
+	 */
 	strz_Ajax.NodeAction.prototype = {
-		init:function(){
+			
+		/**
+		 * Creating needed data containers 
+		 */
+		init : function() {
 			this.__init();
 		},
-		setProperties:function(properties){
-			for(prop in properties){
+
+		/**
+		 * Setting properties
+		 */
+		setProperties : function(properties) {
+			for (prop in properties) {
 				this.__set(prop, properties[prop]);
 			}
 		},
-		run:function(){
-			
+
+		/**
+		 * Override this method in your own object, it should be called after init
+		 */
+		run : function() {
+
 		},
-		ajaxDone:function(data, action) {
-			data=JSON.parse(data);
-			var show=action.getContentShow();
-			
+
+		/**
+		 * Default action after server success response
+		 */
+		ajaxDone : function(data, action) {
+			data = JSON.parse(data);
+			var show = action.getContentShow();
+
 			$(show).html(data.View);
-			$(show).fadeTo('fast',.1);
-			$(show).fadeTo('fast',1);
+			$(show).fadeTo('fast', .1);
+			$(show).fadeTo('fast', 1);
 		},
-		ajaxBeforeSend:function(action){
-			var info=action.getSendInformator();
+
+		/**
+		 * Default action before send request to server 
+		 */
+		ajaxBeforeSend : function(action) {
+			var info = action.getSendInformator();
 			$(info).addClass('ajax-load');
 		},
-		ajaxError:function(err, action) {
+
+		/**
+		 * Default action on after server error response
+		 */
+		ajaxError : function(err, action) {
 			console.log(err);
 			switch (err.status) {
-				case 403:
-					console.log('just login error');
-				default:
-					var content=action.getContentError();
-					$(content).html(err.responseText);
-					break;
+			case 403:
+				console.log('just login error');
+			default:
+				var content = action.getContentError();
+				$(content).html(err.responseText);
+				break;
 			}
 		},
-		ajaxAlways:function(action){
-			var info=action.getSendInformator();
-			if($(info).length>0){
+
+		/**
+		 * Default action on server response, no matter if its success or not
+		 */
+		ajaxAlways : function(action) {
+			var info = action.getSendInformator();
+			if ($(info).length > 0) {
 				$(info).removeClass('ajax-load');
-				
+
 			}
 		},
-		ajaxRun:function(){
-			if(this.isAllowedLocation(document.location.pathname)){
-				var trig=this.getTriggerSelector();
-				if(typeof(trig)!=='undefined'&&$(trig).length>0){
+
+		/**
+		 * Do the request
+		 */
+		ajaxRun : function() {
+			if (this.isAllowedLocation(document.location.pathname)) {
+				var trig = this.getTriggerSelector();
+				if (typeof (trig) !== 'undefined' && $(trig).length > 0) {
 					this.ajaxSetReady();
 				}
 			}
 		},
-		ajaxProperties:{
+
+		/**
+		 * Request properites
+		 */
+		ajaxProperties : {
 			type : null,
 			data : null,
 			url : null,
@@ -60,64 +99,103 @@
 			error : null,
 			beforeSend : null
 		},
-		ajaxInitProperties:function(){
-			this.ajaxProperties['error']=this.ajaxError;
-			this.ajaxProperties['url']=this.getSendToURL();
-			this.ajaxProperties['beforeSend']=this.ajaxBeforeSend;
-			this.ajaxProperties['type']=this.getSendType();
-			this.ajaxProperties['data']=this.getSendData();
+
+		/**
+		 * Initialize properties
+		 */
+		ajaxInitProperties : function() {
+			this.ajaxProperties['error'] = this.ajaxError;
+			this.ajaxProperties['url'] = this.getSendToURL();
+			this.ajaxProperties['beforeSend'] = this.ajaxBeforeSend;
+			this.ajaxProperties['type'] = this.getSendType();
+			this.ajaxProperties['data'] = this.getSendData();
 		},
-		ajaxInitialize:function(){
-			var action=this;
+
+		/**
+		 * Initialize request
+		 */
+		ajaxInitialize : function() {
+			var action = this;
 			this.ajaxInitProperties();
-			var prop={
+			
+			var prop = {
 				type : action.ajaxProperties['type'],
 				url : action.ajaxProperties['url'],
 				cache : action.ajaxProperties['cache'],
-				error : function(err){action.ajaxProperties['error'](err, action)},
-				beforeSend : function(){action.ajaxProperties['beforeSend'](action)},
-				data:$(action.ajaxProperties['data']).serialize()
+				error : function(err) {
+					action.ajaxProperties['error'](err, action)
+				},
+				beforeSend : function() {
+					action.ajaxProperties['beforeSend'](action)
+				},
+				data : $(action.ajaxProperties['data']).serialize()
 			};
-			$.ajax(prop)
-			.done(function(data) {
+			
+			$.ajax(prop).done(function(data) {
 				action.ajaxDone(data, action);
-				if(action.isCallbackMyself()===true)action.ajaxRun();
+				
+				if (action.isCallbackMyself() === true)
+					action.ajaxRun();
+				
 				action.runCallbacks();
-			})
-			.always(function(){	
+				
+			}).always(function() {
 				action.ajaxAlways(action);
 			});
 		},
-		ajaxSetReady:function(){
-			var action=this;	
-			var trig=this.getTriggerSelector();
-			var eventTrigger=this.getEventName();
-			$(trig).unbind(eventTrigger);// prevent double ajax send e.g. in ajax
-		//	var DEBUGGING=true;
-			if(typeof(DEBUGGING)!='undefined'){
-				/*debug*/
+		
+		/**
+		 * Set object is ready for request
+		 */
+		ajaxSetReady : function() {
+			var action = this;
+			var trig = this.getTriggerSelector();
+			var eventTrigger = this.getEventName();
+			/**
+			 * prevent double ajax send
+			 */
+			$(trig).unbind(eventTrigger);
+			
+			/**
+			 * For debugging mode uncomment variable DEBUGGING
+			 */
+			// var DEBUGGING=true;
+			if (typeof (DEBUGGING) != 'undefined') {
+				/* debug */
 				console.log(trig);
 				console.log(action);
 				$(trig).append('*');
-				$(trig).val($(trig).val()+'*');
-				setTimeout(function(){
-					try{
-						$(trig).html($(trig).html().replace('*',''));
-						$(trig).val($(trig).val().replace('*',''));
-					}catch(err){
+				$(trig).val($(trig).val() + '*');
+				setTimeout(function() {
+					try {
+						$(trig).html($(trig).html().replace('*', ''));
+						$(trig).val($(trig).val().replace('*', ''));
+					} catch (err) {
 						console.log('element not exist');
 					}
-				},1000);
-				/*>debug*/
+				}, 1000);
+				/* >debug */
+				
 			}
-			$(trig)[eventTrigger](function(event){
+			
+			/**
+			 * Initialize request
+			 */
+			$(trig)[eventTrigger](function(event) {
 				event.preventDefault();
 				action.ajaxInitialize();
 			});
 		},
+		
+		/**
+		 * Decide what HTTP method to use
+		 */
 		getSendType : function() {
-			return ($(this.getSendData()).serialize())?'POST':'GET';
+			return ($(this.getSendData()).serialize()) ? 'POST' : 'GET';
 		}
 	};
+	/**
+	 * Extend functionality
+	 */
 	strz_Ajax.Extend(strz_Ajax.NodeAction, strz_Ajax.NodePermissionAction);
 })();
